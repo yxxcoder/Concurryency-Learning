e7f624761429772ecb4bd56b9a2754bb4f6723f1
yuxuan <vincent.yuxuan.wang@gmail.com>
2018-11-18 23:58:26 +0800

    thread -- join

--- Chapter1/src/main/java/org/demo/chapter1/recipe5/Main.java
+++ Chapter1/src/main/java/org/demo/chapter1/recipe5/Main.java
@@ -12,6 +12,7 @@ public class Main {
 
     /**
      * 创建并启动FileClock线程用于每秒钟打印一次当前时间，等待5秒后中断线程
+     * 当休眠中线程被中断会立即抛出InterruptedException异常，不需要等待线程休息时间结束
      * @param args
      */
     public static void main(String[] args) {
---
+++ Chapter1/src/main/java/org/demo/chapter1/recipe6/Main.java
@@ -0,0 +1,40 @@
+package org.demo.chapter1.recipe6;
+
+import org.demo.chapter1.recipe6.task.DataSourcesLoader;
+import org.demo.chapter1.recipe6.task.NetworkConnectionsLoader;
+import java.util.Date;
+
+/**
+ * 等待线程的终止
+ * 可以使用Thread类的join()方法达到目的
+ */
+public class Main {
+
+    /**
+     * 创建并启动两个线程，等待他们终止
+     */
+    public static void main(String[] args) {
+
+        // 创建并启动DataSourceLoader线程对象
+        DataSourcesLoader dsLoader = new DataSourcesLoader();
+        Thread thread1 = new Thread(dsLoader,"DataSourceThread");
+        thread1.start();
+
+        // 创建并启动NetworkConnectionsLoader线程
+        NetworkConnectionsLoader ncLoader = new NetworkConnectionsLoader();
+        Thread thread2 = new Thread(ncLoader,"NetworkConnectionLoader");
+        thread2.start();
+
+        // 等待两个线程终止
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // 主线程执行结束
+        System.out.printf("Main: Configuration has been loaded: %s\n",new Date());
+    }
+}
+
