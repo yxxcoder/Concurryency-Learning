084cbd39be7b8e31c194b3169f7da9bc5d2a791d
yuxuan <vincent.yuxuan.wang@gmail.com>
2018-12-30 15:07:28 +0800

    add comment

--- Chapter3/src/main/java/org/demo/chapter3/recipe4/utils/MatrixMock.java
+++ Chapter3/src/main/java/org/demo/chapter3/recipe4/utils/MatrixMock.java
@@ -16,10 +16,11 @@ public class MatrixMock {
      * 传入矩阵行数，每行的长度，要寻找的数字，生成二维数组
      * Constructor of the class. Generates the bi-dimensional array of numbers.
      * While generates the array, it counts the times that appears the number we are going to look for so we can check that the CiclycBarrier class does a good job
+     * 在生成数组时，它计算出我们要查找的数字的次数，这样我们就可以检查CiclycBarrier类是否做得很好
      *
-     * @param size   Number of rows of the array
-     * @param length Number of columns of the array
-     * @param number Number we are going to look for
+     * @param size   Number of rows of the array 数组的行数
+     * @param length Number of columns of the array 数组的列数
+     * @param number Number we are going to look for 要寻找的数字
      */
     public MatrixMock(int size, int length, int number) {
 
