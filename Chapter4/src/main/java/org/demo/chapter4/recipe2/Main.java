7893d66524d2566b41cbc1cc991a02e80c632239
yuxuan <vincent.yuxuan.wang@gmail.com>
2019-07-02 00:02:31 +0800

    ThreadPoolExecutor

--- Chapter4/src/main/java/org/demo/chapter4/recipe2/Main.java
+++ Chapter4/src/main/java/org/demo/chapter4/recipe2/Main.java
@@ -1,11 +1,11 @@
-package org.demo.chapter4.recipe1;
+package org.demo.chapter4.recipe2;
 
-import org.demo.chapter4.recipe1.task.Server;
-import org.demo.chapter4.recipe1.task.Task;
+import org.demo.chapter4.recipe2.task.Server;
+import org.demo.chapter4.recipe2.task.Task;
 
 /**
- * 创建线程执行器
- * 利用 Executors 的 newCachedThreadPool() 方法创建线程执行器
+ * 创建固定大小的线程执行器
+ * 利用 Executors 的 newFixedThreadPool() 方法创建固定大小的线程执行器
  * <p>
  * 模拟一个 Web 服务器来应对来自不同客户端的请求
  * </p>
@@ -26,6 +26,7 @@ public class Main {
         }
 
         server.endServer();
+
     }
 
 }
---
+++ Chapter4/src/main/java/org/demo/chapter4/recipe2/task/Server.java
@@ -0,0 +1,30 @@
+package org.demo.chapter4.recipe2.task;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * 模拟 Web 服务器，接收请求并使用 ThreadPoolExecutor 执行这些请求
+ */
+public class Server {
+
+    private ThreadPoolExecutor executor;
+
+    public Server() {
+        executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(5);
+    }
+
+    public void executeTask(Task task) {
+        System.out.printf("Server: A new task has arrived\n");
+        executor.execute(task);
+        System.out.printf("Server: Pool Size: %d\n", executor.getPoolSize());
+        System.out.printf("Server: Active Count: %d\n", executor.getActiveCount());
+        System.out.printf("Server: Task Count: %d\n", executor.getTaskCount());
+        System.out.printf("Server: Completed Tasks: %d\n", executor.getCompletedTaskCount());
+    }
+
+    public void endServer() {
+        executor.shutdown();
+    }
+
+}
